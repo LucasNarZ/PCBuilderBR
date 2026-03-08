83a43c1c5e5cd38a1d54f0756363351cf2718c18
LucasNarZ <lucasnarloch123@gmail.com>
2026-03-08 17:57:21 -0300

    feat: call job function on schedule

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,22 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from app.jobs import update_products_prices
 
 from app.core.database import engine
 from app.core.database import Base
 from app.routers import components
 
+scheduler = AsyncIOScheduler()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+    scheduler.add_job(update_products_prices, "cron", hour=3, minute=0)
+    scheduler.start()
     yield
+    scheduler.shutdown()
     await engine.dispose()
 
 
