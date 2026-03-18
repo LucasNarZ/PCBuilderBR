820c49795dbaa5190a287b16d4955686f80094d6
lucas <lucasnarloch123@gmail.com>
2026-03-17 23:15:44 -0300

    refactor: export all Specs

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,2 +1,2 @@
 from app.schemas.component import ComponentOfferResponse, ComponentResponse
-from app.schemas.build import BuildComponents, BuildValidation, CompatibilityError
+from app.schemas.build import BuildComponents, BuildValidation, CompatibilityError, ComponentWithChosenOffer, CPUSpecs, MotherboardSpecs, CaseSpecs, CoolerSpecs, MonitorSpecs, StorageSpecs, GPUSpecs, ComponentOffer, RAMSpecs, PSUSpecs
