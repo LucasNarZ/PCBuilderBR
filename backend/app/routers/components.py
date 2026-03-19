b5b6c86293f005f9fc7970b8a5f794e4986ab60d
lucas <lucasnarloch123@gmail.com>
2026-03-19 20:44:12 -0300

    fix

--- backend/app/routers/components.py
+++ backend/app/routers/components.py
@@ -10,7 +10,7 @@ async def get_components(part_type: str = Query(None), query: str = Query(None),
     return await service.list(part_type, query)
 
 @router.get("/{component_name}/offers")
-async def get_component_offers(component_name: str, service: ComponentService = Depends(get_component_service)) -> list[ComponentOffer]:
+async def get_component_offers(component_name: str, service: ComponentService = Depends(get_component_service)) -> list[ComponentOfferResponse]:
     return await service.list_cheapest_offers_by_store(component_name)
 
 
