098d07a6808994bfad15d3931bf204a3e3afe50b
lucas <lucasnarloch123@gmail.com>
2026-03-27 21:15:59 -0300

    feat: CD pipeline

---
+++ scripts/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+cd ~/PCBuilderBR
+
+git fetch origin
+git reset --hard origin/main
+
+aws ecr get-login-password --region us-east-1 \
+| docker login --username AWS --password-stdin 242201312839.dkr.ecr.us-east-1.amazonaws.com
+
+docker compose -f docker-compose.prod.yml pull
+
+docker compose -f docker-compose.prod.yml down
+
+docker compose -f docker-compose.prod.yml up -d
+
+cd backend
+source .venv/bin/activate
+uv sync
+alembic upgrade head
+
+
